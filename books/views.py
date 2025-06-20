65afd115582e77605f53c36f09e524abe348efe0
Kiprotich Code <fleeklawi@gmail.com>
2025-06-20 23:44:24 +0300

    merge-conflict: minor fix

--- books/views.py
+++ books/views.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 from django.shortcuts import render
->>>>>>> 1ca8d3491e7a5bec56398597c69416f09f81d570
 from rest_framework import viewsets
 from .models import Book, Genre
 from .serializers import BookSerializer, GenreSerializer
@@ -9,11 +6,7 @@ from .serializers import BookSerializer, GenreSerializer
 # Create your views here.
 class BookViewset(viewsets.ModelViewSet):
     """
-<<<<<<< HEAD
     Performing CRUD on books
-=======
-    Viewset to perform CRUD on Books
->>>>>>> 1ca8d3491e7a5bec56398597c69416f09f81d570
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -24,8 +17,4 @@ class GenreViewset(viewsets.ModelViewSet):
     Perform CRUD on Genres
     """
     queryset = Genre.objects.all()
-<<<<<<< HEAD
     serializer_class = GenreSerializer
-=======
-    serializer_class = GenreSerializer
->>>>>>> 1ca8d3491e7a5bec56398597c69416f09f81d570
